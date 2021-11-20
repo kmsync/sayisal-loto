4860f0019a528affcdb8c15951ccb134b0345233
kmsync <tommyjwi@gmail.com>
2021-11-20 12:39:28 +0300

    Çoklu Şart ve While eklendi.

--- src/com/bilgeadam/sayisalLoto/CokluSartOrnek.java
+++ src/com/bilgeadam/sayisalLoto/CokluSartOrnek.java
@@ -6,15 +6,21 @@ public class CokluSartOrnek {
 
         int sayi1 = 23;
         int sayi2 = 35;
-        int sayi3 = 23;
+        int sayi3 = 41;
 
         // veya(or) operatörü ||
+        // ve(and) operatörü &&
         if (sayi3 == sayi1 || sayi3 == sayi2) {
 
             System.out.println("Tekrarlanan sayı var!");
 
         }
 
+        while (sayi3 != sayi1 && sayi3 != sayi2) {
+            System.out.println("Sayı3 diğerlerine eşit değil!");
+
+            sayi3 = sayi1;
+        }
 
     }
 
